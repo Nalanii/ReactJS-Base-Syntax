aec0dbbcbe0433bcf96605fc392a9de56a91eec1
nstory <nstory@peraton.com>
2020-05-22 12:09:34 -0400

    Add Style

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 const userInput = (props) => {
+  const style = {
+    border: "2px solid red",
+  };
   return (
     <div>
-      <input type="text" onChange={props.changed} value={props.username} />
+      <input
+        type="text"
+        style={style}
+        onChange={props.changed}
+        value={props.username}
+      />
     </div>
   );
 };
